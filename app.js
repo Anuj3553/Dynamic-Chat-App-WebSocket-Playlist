1353d1f443429f30effbc4e8df70b405e16ac6ca
Anuj3553 <anujverma3553@gmail.com>
2025-05-28 14:11:34 +0530

    How to Update the User Online Offline Status using SocketIO

--- app.js
+++ app.js
@@ -8,9 +8,44 @@ const app = require('express')();
 
 const http = require('http').Server(app);
 
-const userRoute = require('./routes/userRoute.js')
+const userRoute = require('./routes/userRoute.js');
+const User = require('./models/userModel.js');
 
 app.use('/', userRoute)
+
+const io = require('socket.io')(http);
+
+var usp = io.of('/user-namespace')
+
+usp.on('connection', async function (socket) {
+    console.log("user connected");
+
+    let userId = socket.handshake.auth.token;
+
+    await User.findByIdAndUpdate(
+        { _id: userId },
+        {
+            $set: {
+                is_online: '1'
+            }
+        }
+    )
+
+    socket.on('disconnect', async function () {
+        console.log("user disconnected");
+
+        let userId = socket.handshake.auth.token;
+
+        await User.findByIdAndUpdate(
+            { _id: userId },
+            {
+                $set: {
+                    is_online: '0'
+                }
+            }
+        )
+    });
+});
 
 http.listen(3000, function () {
     console.log('Server is running on port http://localhost:3000/');
